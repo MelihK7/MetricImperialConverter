dbe00c0c387a155d089a95998b99730d99ecd467
Melih Kelkitli <s235114@dtu.dk>
2026-01-09 19:59:03 +0100

    Commit

--- index.js
+++ index.js
@@ -4,3 +4,28 @@
 1 kilogram = 2.204 pound
 */
 
+const inputEl = document.getElementById("input-el")
+const lengthP = document.getElementById("LengthP")
+const convertBtn = document.getElementById("convert-Btn")
+
+convertBtn.addEventListener("click",function(){
+
+    if(inputEl.value){
+    calculate()
+    } else {
+        console.log("Enter a number")
+    }
+})
+
+function calculate() {
+    let input = 0;
+
+    input = Number(inputEl.value)
+
+
+    let metersToFeet = Number(input * 3.281).toFixed(3)
+    let feetToMeters = Number(input * 0.3048).toFixed(3)
+
+    lengthP.innerHTML = `${input} meters = ${feetToMeters} | ${input} feet = ${feetToMeters}`
+}
+
