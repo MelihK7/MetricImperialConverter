a5a004e63c2c0d0ca7f0ca48e83162068e61cfbc
Melih Kelkitli <s235114@dtu.dk>
2026-01-09 20:03:50 +0100

    commit

--- index.js
+++ index.js
@@ -5,7 +5,11 @@
 */
 
 const inputEl = document.getElementById("input-el")
+
 const lengthP = document.getElementById("LengthP")
+const volumeP = document.getElementById("VolumeP")
+const massP = document.getElementById("MassP")
+
 const convertBtn = document.getElementById("convert-Btn")
 
 convertBtn.addEventListener("click",function(){
@@ -22,10 +26,17 @@ function calculate() {
 
     input = Number(inputEl.value)
 
-
+    // Length (Meter/feet) section
     let metersToFeet = Number(input * 3.281).toFixed(3)
     let feetToMeters = Number(input * 0.3048).toFixed(3)
+    lengthP.innerHTML = `${input} meters = ${feetToMeters} | ${input} feet = ${metersToFeet} `
 
-    lengthP.innerHTML = `${input} meters = ${feetToMeters} | ${input} feet = ${feetToMeters}`
-}
+    //Volume (Liters/Gallons) section
+    let litersToGallons = Number(input * 0.264).toFixed(3)
+    let gallonsToLiters = Number(input * 0.3048).toFixed(3)
+    lengthP.innerHTML = `${input} meters = ${feetToMeters} | ${input} feet = ${metersToFeet} `
+
+    //Mass (Kilograms/Pounds) section
+    
+}   
 
